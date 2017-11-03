fd7091a914a050424bc91eef6ebfaea651c1906b
Andreas Holmstedt <holmstedt.andreas@hotmail.com>
2017-11-03 09:24:55 +0100

    first changes

---
+++ blocket.js
@@ -0,0 +1,40 @@
+
+function advertBigger1() {
+    var x = document.getElementById("advert1");
+    
+    
+    if(x.style.height === "16em") {
+        x.style.height = "40em";
+    }else{
+        x.style.height = "16em";
+    }
+}
+
+
+
+
+
+function advertBigger2() {
+    var x = document.getElementById("advert2");
+    
+    
+    if(x.style.height === "16em") {
+        x.style.height = "40em";
+    }else{
+        x.style.height = "16em";
+    }
+}
+
+
+
+
+function advertBigger3() {
+    var x = document.getElementById("advert3");
+    
+    
+    if(x.style.height === "16em") {
+        x.style.height = "40em";
+    }else{
+        x.style.height = "16em";
+    }
+}
